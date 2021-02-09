2863d3a1cb3b0da7c22aecce90f72e73955d52f1
albulacan <albulacan@teligent.com.ph>
2021-02-09 10:46:53 +0800

    added to-do-app project

---
+++ to-do-app/src/main/java/com/springboot/bootcamp/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
