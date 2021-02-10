e59a670db3cf1f2ebf5f207a7c4110445d004bc9
albulacan <albulacan@teligent.com.ph>
2021-02-10 09:38:44 +0800

    refactor

--- to-do-app/src/main/java/com/springboot/bootcamp/ToDoApplication.java
+++ to-do-app/src/main/java/com/springboot/bootcamp/ToDoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class ToDoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToDoAppApplication.class, args);
+		SpringApplication.run(ToDoApplication.class, args);
 	}
 
 }
